a84db2660d69207f3f3d691068835627a3093726
JoaoZalen <joaozplays2@gmail.com>
2026-09-25 14:43:51 -0300

    Remove migracao antiga de categorias

--- migration_v2_categories.sql
+++
@@ -1,8 +0,0 @@
-USE taskflow_lite;
-
--- Use este arquivo somente para atualizar manualmente uma instalação anterior.
-ALTER TABLE tasks
-    ADD COLUMN IF NOT EXISTS category VARCHAR(60) NULL AFTER description;
-
-ALTER TABLE tasks
-    ADD INDEX IF NOT EXISTS idx_tasks_user_category (user_id, category);
